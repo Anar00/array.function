d768a4d9a635fab86e90812021ad46dc4dd54181
Anar00 <anarabbasov617@gmail.com>
2022-11-03 12:03:40 +0400

    array

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+int [] number = { 1, 2, 3, 4 };
+
+
+for (int a = 0; a < number.Length; a++)
+{
+    Console.WriteLine(number[a]);
+}
+for (int a=0; a <number.Length; a++)
+{
+    Console.WriteLine(number[a]+ 1);
+   
+}
---
+++ array.js
@@ -0,0 +1,11 @@
+let  number = [1,2,3,4];
+
+for(let a=0 ; a < number.length; a++)
+{
+    console.log(number[a]);
+}
+
+for(let a=0 ; a < number.length; a++)
+{
+console.log(number[a]+1);
+}
